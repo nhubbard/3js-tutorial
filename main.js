180ad757d0e9b722041f7c26ad16d64d79d55e2b
Nicholas Hubbard <nhubbard@users.noreply.github.com>
2024-11-06 18:55:06 -0500

    Step 4: Set up Three.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+import {
+  WebGLRenderer,
+} from "three";
 import './main.css';
+
+// Step 4: Get the canvas element to render into, and create WebGL renderer for it
+const canvas = document.querySelector("#c");
+const renderer = new WebGLRenderer({ antialias: true, canvas });
