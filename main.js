ee0c09350dee970dcba01cb21b243a1b52b61809
Nicholas Hubbard <nhubbard@users.noreply.github.com>
2024-11-06 18:56:19 -0500

    Step 5a: Set camera constants

--- main.js
+++ main.js
@@ -6,3 +6,9 @@ import './main.css';
 // Step 4: Get the canvas element to render into, and create WebGL renderer for it
 const canvas = document.querySelector("#c");
 const renderer = new WebGLRenderer({ antialias: true, canvas });
+
+// Step 5a: Set the camera constants that define how our camera will look at our objects
+const fov = 75;
+const aspect = 2;
+const near = 0.1;
+const far = 5;
